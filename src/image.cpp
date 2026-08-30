2e8464f30f6f84e1cf09ebc5ab06ab3a80982ea3
boyjayy <popkov.ad@icloud.com>
2026-08-30 17:44:22 +0300

    type fixes and base for bilinear interpolation

--- src/image.cpp
+++ src/image.cpp
@@ -28,9 +28,19 @@ GrayScale resize_grayscale(const GrayScale& image, std::size_t new_width, std::s
     resized_image.height = new_height;
     resized_image.data.resize(new_width * new_height);
     //bilinear interpolation 
-    size_t x_ratio = static_cast<size_t>(image.width) / new_width;
-    size_t y_ratio = static_cast<size_t>(image.height) / new_height;
+    double x_ratio = static_cast<double>(image.width) / new_width;
+    double y_ratio = static_cast<double>(image.height) / new_height;
+    //x = a*(1-t) + b*t in 1 dim  t is param that we can change
+    // in 2nd dim we calc the same way as a 1 dim at first for points  x1y1 x2y1 and for x1y2 x2y2
+    // then we got 2 points instead of 4 so we can make LERP(linear interpolation xd) between them to get the final point
+    // https://en.wikipedia.org/wiki/Bilinear_interpolation
     for (std::size_t i = 0; i < new_height; i++) {
-
+        for (std::size_t j = 0; j < new_width ; j++) {
+            size_t x = j*x_ratio;
+            size_t y = i*y_ratio;
+            size_t x_diff = j*x_ratio - x;
+            size_t y_diff = i*y_ratio - y;
+            
+        }
     }
 }
