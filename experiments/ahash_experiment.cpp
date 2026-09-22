bddc8815c93db4c2579128b0f1014c7247b8b83d
boyjayy <popkov.ad@icloud.com>
2026-09-22 14:03:51 +0300

    added col source for output

--- experiments/ahash_experiment.cpp
+++ experiments/ahash_experiment.cpp
@@ -19,21 +19,21 @@ signed main() {
     const fs::path original_path = dir / "original.jpeg";
     imgsim::ImgHash orig_hash = imgsim::average_hash(original_path);
     std::vector<Exp> pics = {
-        {"copy", dir / "copy.jpeg",       1},
-        {"copy2",dir / "copy copy.jpeg",  1},
-        {"resized",dir / "resized.jpeg",    1},
-        {"compressed",dir / "compress.jpeg",   1},
-        {"bright",dir / "bright.jpg",      1},
-        {"dark", dir / "dark.jpg",        1},
-        {"crop",dir / "crop.jpeg",       1},
-        {"rotate", dir / "rotate.jpeg",     1},
+        {"copy", dir / "copy.jpeg", 1},
+        {"copy2",dir / "copy copy.jpeg", 1},
+        {"resized",dir / "resized.jpeg", 1},
+        {"compressed",dir / "compress.jpeg",1},
+        {"bright",dir / "bright.jpg", 1},
+        {"dark", dir / "dark.jpg", 1},
+        {"crop",dir / "crop.jpeg",  1},
+        {"rotate", dir / "rotate.jpeg", 1},
         {"different1",dir / "different1.jpeg", 0},
         {"different2", dir / "different2.jpeg", 0},
         {"different3", dir / "different3.jpeg", 0},
         {"different4", dir / "different4.jpeg", 0},
         {"different5", dir / "different5.jpeg", 0},
     };
-    std::cout << std::left << std::setw(15) <<"image"<<"distance\n";
+    std::cout << std::left << std::setw(15) <<"image"<<std::setw(15)<<"source"<<"distance\n";
     for (auto& pic: pics) {
         imgsim::ImgHash pichash = imgsim::average_hash(pic.path);
         std::uint32_t d = imgsim::hamming_distance(orig_hash, pichash);
